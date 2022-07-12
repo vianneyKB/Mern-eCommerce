240c1e72205f15ec00162fdc47854817c8fdab59
vianneyKB <36236520+vianneyKB@users.noreply.github.com>
2022-07-13 00:57:48 +0200

    AB#4 setting up data from the backend in json format

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const products = require('./data/products')
+
+const app = express()
+
+// app.get('/', (req, res) => {
+//     res.send('API is runing...')
+// })
+
+// creating a root to get data by _id
+app.get('/api/products', (req, res) => {
+    res.json(products)
+})
+// creating a routes to get data by _id
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find((p) => p._id === req.params.id)
+    res.json(product)
+})
+
+app.listen(5000, console.log('server listening on port 5000'))
