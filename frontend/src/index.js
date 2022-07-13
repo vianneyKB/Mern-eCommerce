55e1b3f1bd6268ba63a4397b387dc9d815c702f0
vianneyKB <36236520+vianneyKB@users.noreply.github.com>
2022-07-13 02:15:32 +0200

    AB#5 Getting data from products on the server side to the client side

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 //import * as serviceWorker from './serviceWorker'
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+// import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+// import products from '../products'
+import axios from 'axios'
 
 const ProductScreen = ({match}) => {
-    const { id } = useParams();
-    const product = products.find((p) => p._id === (id));
+    // const { id } = useParams();
+    // const product = products.find((p) => p._id === (id));
+    const [product, setProduct] = useState({})
+    // const { id } = useParams()
+
+    useEffect(() => {
+      const fetchProduct = async () => {
+        const {data} = await axios.get(`/api/products/${match.params.id}`)
+    
+        setProduct(data)
+      }
+      fetchProduct()
+    }, [])
+
 
   return <>
     <Link className="btn btn-dark my-3" to='/'>
